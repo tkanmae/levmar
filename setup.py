44d55a8c995a47756906f8b278a3fbf3fc6bee52
Takeshi Kanmae <tkanmae@gmail.com>
2010-05-27 21:32:57 -0800

    Use of `add_library()`
    Now levmar C-library is not compiled every build time.  It will reduce
    the total build time.

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 TODO:
-    * Isolate the extension build from `levmar` library build in order
-      to reduce compile time.
     * Run `nosetest` as an Git pre-commit hook.
 """
 import sys
@@ -12,47 +10,43 @@ import os
 root_dir = 'levmar'
 version = '0.10'
 src_dir = os.path.join(root_dir, 'src')
+lib_dir = os.path.join(src_dir, 'levmar-2.5')
 
 
-def levmar_config():
-    levmar_dir = os.path.join(src_dir, 'levmar-2.5')
-    sources = ('lm.c', 'Axb.c', 'misc.c', 'lmlec.c',
-               'lmbc.c', 'lmblec.c', 'lmbleic.c')
-    sources = [os.path.join(levmar_dir, f) for f in sources]
-    include_dirs = [levmar_dir]
-    return sources, include_dirs
+def extention_src():
+    """Return a list containing the paths to the extension source files"""
+    src = ['levmar.c']
+    src = [os.path.join(src_dir, f) for f in src]
+    include_dirs = [src_dir, lib_dir]
+    return src, include_dirs
 
 
-def get_lapack_opts():
-    from numpy.distutils.system_info import get_info
-    return get_info('lapack_opt')
+def library_src():
+    """Return a list containing the paths to the C-library source files"""
+    src = ('lm.c', 'Axb.c', 'misc.c', 'lmlec.c',
+           'lmbc.c', 'lmblec.c', 'lmbleic.c')
+    src = [os.path.join(lib_dir, f) for f in src]
+    return src
 
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.system_info import get_info
 
     config = Configuration('levmar', parent_package, top_path,
                            package_path=root_dir)
 
-    # -- Add `_levmar` extension module.
-    sources = ['levmar.c']
-    sources = [os.path.join(src_dir, f) for f in sources]
-    include_dirs = [src_dir]
-    ## levmar sources and include_dirs
-    levmar_sources, levmar_include_dirs = levmar_config()
-    sources.extend(levmar_sources)
-    include_dirs.extend(levmar_include_dirs)
+    # -- Add `levmar` C library
+    config.add_library('levmar', sources=library_src())
 
+    # -- Add `levmar` extension module.
+    src, inc_dirs = extention_src()
+    lapack_opts = get_info('lapack_opt')
     config.add_extension('levmar',
-                         sources=sources,
-                         include_dirs=include_dirs,
-                         extra_info=get_lapack_opts())
-    # config.add_extension('levmar',
-    #                      sources=sources,
-    #                      include_dirs=include_dirs,
-    #                      extra_info=get_lapack_opts(),
-    #                      extra_compile_args=['-g'],
-    #                      extra_link_args=['-g'])
+                         sources=src,
+                         include_dirs=inc_dirs,
+                         libraries=['levmar'],
+                         extra_info=lapack_opts)
 
     # -- Add `tests` directory.
     config.add_data_dir(('tests', os.path.join(root_dir, 'tests')))
