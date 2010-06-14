f2e3085ad70157b21d238cb5415e9067fa7c17a9
Takeshi Kanmae <tkanmae@gmail.com>
2010-06-14 12:48:36 -0800

    Refactor test code

--- lvmr/tests/test_interface.py
+++ lvmr/tests/test_interface.py
@@ -11,6 +11,32 @@ import lvmr
 from lvmr import LMUserFuncError
 
 
+class TestDataErrors(TestCase):
+
+    def test_not_same_size(self):
+        assert_raises(ValueError,
+                      lvmr.Data, np.arange(100), np.arange(101))
+        assert_raises(ValueError,
+                      lvmr.Data, np.arange(100), np.arange(100), np.arange(101))
+
+
+class TestModelErrors(TestCase):
+
+    def setUp(self):
+        self.x = np.arange(10, dtype=np.float64)
+        self.y = np.arange(10, dtype=np.float64)
+        self.func = lambda p, x: p[0]*x + p[1]
+        self.p0 = (1, 1)
+
+    def test_not_callable(self):
+        invalid_funcs = (1, 'foo', [1, 2], (1, 2), {'foo': 1})
+        for f in invalid_funcs:
+            ## `func`
+            assert_raises(TypeError, lvmr.Model, f)
+            ## `jacf`
+            assert_raises(TypeError, lvmr.Model, self.func, jacf=f)
+
+
 class TestFuncErrors(TestCase):
 
     def setUp(self):
