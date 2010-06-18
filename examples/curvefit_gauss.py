de1005ae437b999cb6307ffadbbf57690a85ac60
Takeshi Kanmae <tkanmae@gmail.com>
2010-06-18 11:42:49 -0800

    More accurate description of `bounds`
    Tuning the initial estimates and the constraints, so that the widths
    cannot be equal to zero.

--- examples/curvefit_gauss.py
+++ examples/curvefit_gauss.py
@@ -3,49 +3,55 @@
 # ----------------------------------------------------------------------
 # Copyright (c) 2010 Takeshi Kanmae
 # ----------------------------------------------------------------------
-"""Demostration of use of box constraints.
+"""Demonstration of use of box constraints.
 
-Curve ftting with a Gauss function.
+Curve fitting with a Gauss function.
 
 
-A keyword parameter `bounds` is for box constraints.  `bounds` must be a
-tuple/list consisting of tuples of two floats/None or Nones.  Its length
-must be the same as the parameters.  A tuple specifies the lower and
-upper bound for a parameter, and a None means no constraint.  If one of
-two values in a tuple is None, then the bound is semi-definite.  For
-example,
+A keyword parameter `bounds` is for box constraints lb[i]<=p[i]<=ub[i].
+`bounds` must be a tuple/list, and its length must be the same as the
+parameters.  A constraint for a parameter consists of a tuples of two
+floats/Nones or Nones.  A tuple determines the lower and upper bound,
+and a None means no constraint.  If one of two values in a tuple is
+None, then the bound is semi-definite.  For example,
 
     >>> bounds = [None, (-10, 3), (None, 10), (0, None)]
 
 specifies that the first parameter has no constraint, the second has -10
 and 3 for the lower and upper bound respectively, the third has only the
-upper bound of 10, and the firth has to be larger than 0.
+upper bound of 10, and the fourth has to be greater than or equal to 0.
 """
+from math import *
 import numpy as np
 try:
     from matplotlib import pyplot as plt
     has_mpl = True
 except ImportError:
     has_mpl = False
-
 import lvmr
+
+## If you prefer to reproduce the result, set a seed to the generator.
+# np.random.seed(1234)
+
+__4ln2 = 4 * log(2)
 
 
 def gauss(p, x):
-    y = p[0] * np.exp(-((x-p[1])/p[2])**2) + p[3]
+    y = p[0] * np.exp(-__4ln2*((x-p[1])/p[2])**2) + p[3]
     return y
 
 
+## Create input data
 x = np.linspace(-5, 5)
 pt = [250, -0.4, 2.0, 0.1]
 yt = gauss(pt, x)
 y = yt + np.sqrt(yt) * np.random.randn(x.size)
 
-p0 = [1.0, -3.0, 3.0, 1]
-
-## Ensure the width is positive.
-bounds = [None, None, (0, None), None]
-
+## Initial estimate
+p0 = [1.0, -3.0, 3.0, 1.0]
+## Ensure the width is (0, Inf).
+bounds = [None, None, (1e-6, None), (-10, 10)]
+## Run fitting routine
 ret = lvmr.levmar(gauss, p0, y, args=(x,), bounds=bounds)
 
 print ':Expected:'
@@ -53,11 +59,11 @@ print pt
 print ':Estimate:'
 print ret.p
 print
-
+## Print summary of the fitting
 print ' Summary '.center(60, '*')
 print ret
 print ''.center(60, '*')
-
+## Plot the result
 if has_mpl:
     plt.plot(x, y, 'bo')
     plt.plot(x, yt, 'b-', label='true')
--- examples/curvefit_psdvoigt.py
+++ examples/curvefit_psdvoigt.py
@@ -3,22 +3,23 @@
 # ----------------------------------------------------------------------
 # Copyright (c) 2010 Takeshi Kanmae
 # ----------------------------------------------------------------------
-"""Demostration of use of box constraints.
+"""Demonstration of use of box constraints.
 
-Curve ftting with a psuedo-Voigt function.
+Curve fitting with a psuedo-Voigt function.
 
-A keyword parameter `bounds` is for box constraints.  `bounds` must be a
-tuple/list consisting of tuples of two floats/None or Nones.  Its length
-must be the same as the parameters.  A tuple specifies the lower and
-upper bound for a parameter, and a None means no constraint.  If one of
-two values in a tuple is None, then the bound is semi-definite.  For
-example,
+
+A keyword parameter `bounds` is for box constraints lb[i]<=p[i]<=ub[i].
+`bounds` must be a tuple/list, and its length must be the same as the
+parameters.  A constraint for a parameter consists of a tuples of two
+floats/Nones or Nones.  A tuple determines the lower and upper bound,
+and a None means no constraint.  If one of two values in a tuple is
+None, then the bound is semi-definite.  For example,
 
     >>> bounds = [None, (-10, 3), (None, 10), (0, None)]
 
 specifies that the first parameter has no constraint, the second has -10
 and 3 for the lower and upper bound respectively, the third has only the
-upper bound of 10, and the firth has to be larger than 0.
+upper bound of 10, and the fourth has to be greater than or equal to 0.
 """
 from math import *
 import numpy as np
@@ -27,9 +28,10 @@ try:
     has_mpl = True
 except ImportError:
     has_mpl = False
-
 import lvmr
 
+## If you prefer to reproduce the result, set a seed to the generator.
+# np.random.seed(1234)
 
 __4ln2 = 4 * log(2)
 
@@ -42,16 +44,17 @@ def psd_voigt(p, x):
     return peak * (((1-m)*g + m*l) / c) + const
 
 
+## Create input data
 x = np.linspace(-6, 6)
-pt = [250, -0.4, 2.0, 0.4, 0.1]
+pt = [260, -0.4, 2.0, 0.4, 0.1]
 yt = psd_voigt(pt, x)
 y = yt + np.sqrt(yt) * np.random.randn(x.size)
 
-p0 = [100, -3.0, 3.0, 0.2, 0.2]
-
-## Ensure the width is positive, and the mixing ratio is [0, 1].
-bounds = [None, None, (0, None), (0, 1), (-1, 1)]
-
+## Initial estimate
+p0 = [100, -3.0, 3.0, 0.2, 0.5]
+## Ensure the width is (0, Inf), and the mixing ratio is [0, 1].
+bounds = [(None, 5e+2), None, (1e-6, None), (0, 1), (-10, 10)]
+## Run fitting routine
 ret = lvmr.levmar(psd_voigt, p0, y, args=(x,), bounds=bounds)
 
 print ':Expected:'
@@ -59,11 +62,11 @@ print pt
 print ':Estimate:'
 print ret.p
 print
-
+## Print summary of the fitting
 print ' Summary '.center(60, '*')
 print ret
 print ''.center(60, '*')
-
+## Plot the result
 if has_mpl:
     plt.plot(x, y, 'bo')
     plt.plot(x, yt, 'b-', label='true')
