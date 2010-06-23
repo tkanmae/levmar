4376ae3ec6374495b353d08e052b8afa2cc19a3e
Takeshi Kanmae <tkanmae@gmail.com>
2010-06-23 11:31:06 -0800

    Rearrange the directory trees

--- setup.py
+++ setup.py
@@ -5,28 +5,26 @@
 # ----------------------------------------------------------------------
 import sys
 import os
+from os.path import join as pjoin
 
-root_dir = 'lvmr'
+package_path = 'lvmr'
 version = '0.10'
-src_dir = os.path.join(root_dir, 'src')
-lib_dir = 'levmar-2.5'
+library_dir = 'levmar-2.5'
 
 
-def extention_src():
-    """Return a list containing the paths to the extension source
-    files"""
-    src = ['_lvmr.c']
-    src = [os.path.join(src_dir, f) for f in src]
-    include_dirs = [src_dir, lib_dir]
-    return src, include_dirs
+def get_extension_sources():
+    src = ('_lvmr.c',)
+    return [pjoin(package_path, f) for f in src]
 
 
-def library_src():
-    """Return a list containing the paths to the C levmar library source
-    files"""
-    src = ('lm.c', 'Axb.c', 'misc.c', 'lmlec.c',
-           'lmbc.c', 'lmblec.c', 'lmbleic.c')
-    src = [os.path.join(lib_dir, f) for f in src]
+def get_extension_include_dirs():
+    return [package_path, library_dir]
+
+
+def get_library_sources():
+    src = ('lm.c', 'Axb.c', 'misc.c', 'lmlec.c', 'lmbc.c', 'lmblec.c',
+           'lmbleic.c',)
+    src = [pjoin(library_dir, f) for f in src]
     return src
 
 
@@ -34,23 +32,24 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info
 
-    config = Configuration('lvmr', parent_package, top_path,
-                           package_path=root_dir)
+    config = Configuration('lvmr',
+                           parent_package,
+                           top_path,
+                           package_path=package_path)
 
     ## Add `levmar` C library
-    config.add_library('levmar', sources=library_src())
+    config.add_library('levmar',
+                       sources=get_library_sources())
 
     ## Add `levmar` extension module.
-    src, inc_dirs = extention_src()
-    lapack_opts = get_info('lapack_opt')
     config.add_extension('_lvmr',
-                         sources=src,
-                         include_dirs=inc_dirs,
+                         sources=get_extension_sources(),
+                         include_dirs=get_extension_include_dirs(),
                          libraries=['levmar'],
-                         extra_info=lapack_opts)
+                         extra_info=get_info('lapack_opt'),)
 
     ## Add `tests` directory.
-    config.add_data_dir(('tests', os.path.join(root_dir, 'tests')))
+    config.add_data_dir(('tests', pjoin(package_path, 'tests')))
 
     return config
 
