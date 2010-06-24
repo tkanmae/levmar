353b9afc86900c4a9d3abe4f2b79acd2c2b69b91
Takeshi Kanmae <tkanmae@gmail.com>
2010-06-24 14:09:42 -0800

    Add tests for _LMBoxConstraints

---
+++ lvmr/tests/test_boxconstraints.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.6
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Copyright (c) 2010 Takeshi Kanmae
+# ----------------------------------------------------------------------
+from __future__ import division
+import numpy as np
+from numpy.testing import *
+
+from lvmr._lvmr import (_LMBoxConstraints)
+
+
+def test_valid1():
+    m = 2
+    bounds = [(0, 1), (2, 3)]
+    assert_(isinstance(_LMBoxConstraints(bounds, m), _LMBoxConstraints))
+
+
+def test_valid2():
+    m = 2
+    bounds = [(0, None), None]
+    assert_(isinstance(_LMBoxConstraints(bounds, m), _LMBoxConstraints))
+
+
+def test_valid3():
+    m = 2
+    bounds = [(None, 0), None]
+    assert_(isinstance(_LMBoxConstraints(bounds, m), _LMBoxConstraints))
+
+
+def test_valid4():
+    m = 2
+    bounds = [(0, np.inf), None]
+    assert_(isinstance(_LMBoxConstraints(bounds, m), _LMBoxConstraints))
+
+
+def test_valid5():
+    m = 2
+    bounds = [(-np.inf, 0), None]
+    assert_(isinstance(_LMBoxConstraints(bounds, m), _LMBoxConstraints))
+
+
+def test_invalid_shape1():
+    m = 2
+    bounds = [(0, 1), (2, 3), (3, 4)]
+    assert_raises(ValueError, _LMBoxConstraints, bounds, m)
+
+
+def test_invalid_shape2():
+    m = 2
+    bounds = [(0, 1)]
+    assert_raises(ValueError, _LMBoxConstraints, bounds, m)
+
+
+def test_invalid_value1():
+    m = 2
+    bounds = [1, 2]
+    assert_raises(ValueError, _LMBoxConstraints, bounds, m)
+
+
+def test_invalid_value2():
+    m = 2
+    bounds = [1, (2, 3)]
+    assert_raises(ValueError, _LMBoxConstraints, bounds, m)
+
+
+def test_invalid_value3():
+    m = 2
+    bounds = [(), (2, 3)]
+    assert_raises(ValueError, _LMBoxConstraints, bounds, m)
+
+
+
+if __name__ == '__main__':
+    run_module_suite()
--- lvmr/tests/test_core.py
+++ lvmr/tests/test_core.py
@@ -1039,40 +1039,5 @@ class TestFuncErrors(TestCase):
                       args=(self.x, ()))
 
 
-class TestBCErrors(TestCase):
-
-    def setUp(self):
-        self.x = np.arange(10, dtype=np.float64)
-        self.y = np.arange(10, dtype=np.float64)
-        self.func = lambda p, x: p[0]*x + p[1]
-        self.p0 = (1, 1)
-
-    def test_not_valid_type(self):
-        invalid_bounds = [0, 'bounds']
-        for bounds in invalid_bounds:
-            assert_raises(TypeError,
-                          run_levmar, self.func, self.p0, self.y,
-                          args=(self.x,), bounds=bounds)
-
-    def test_not_valid_size(self):
-        invalid_bounds = [
-            [None,],
-            [None, None, None,],
-            [(0,2), (0,2), (0,2)],
-            [(0,2), (0,2,3)],
-            [tuple(), (0,2)]]
-        for bounds in invalid_bounds:
-            assert_raises(ValueError,
-                          run_levmar, self.func, self.p0, self.y,
-                          args=(self.x,), bounds=bounds)
-
-    def test_not_valid_value(self):
-        invalid_bounds = [[None, (None,'upper')],]
-        for bounds in invalid_bounds:
-            assert_raises(ValueError,
-                          run_levmar, self.func, self.p0, self.y,
-                          args=(self.x,), bounds=bounds)
-
-
 if __name__ == '__main__':
     run_module_suite()
