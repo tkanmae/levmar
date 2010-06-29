57a74d618a64e09b6207441b9fcd707b9126eec6
Takeshi Kanmae <tkanmae@gmail.com>
2010-06-28 21:09:23 -0800

    Use of a exponential function

--- lvmr/tests/test_LMFunction.py
+++ lvmr/tests/test_LMFunction.py
@@ -11,24 +11,25 @@ from lvmr._levmar import (_LMFunction, __py_verify_funcs, LMUserFuncError, )
 
 
 def test_valid_func1():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
-    func = lambda p, x : p[0]*x + p[1]
+    func = lambda p, x : p[0]*np.exp(-p[1]*x) + p[2]
 
     lm_func = _LMFunction(func, args=(x,))
     assert_(__py_verify_funcs(lm_func, p, m, n))
 
 
 def test_valid_func2():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
-    func = lambda p, x : p[0]*x + p[1]
+    func = lambda p, x : p[0]*np.exp(-p[1]*x) + p[2]
     def jacf(p, x):
         y = np.empty((n,m))
-        y[:,0] = x
-        y[:,1] = 1
+        y[:,0] = np.exp(-p[1]*x)
+        y[:,1] = -p[0] * x * np.exp(-p[1]*x)
+        y[:,2] = np.ones(x.size)
         return y
 
     lm_func = _LMFunction(func, args=(x,),  jacf=jacf)
@@ -36,35 +37,35 @@ def test_valid_func2():
 
 
 def test_valid_method():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
 
     class Foo(object):
         def func(self, p, x):
-            return p[0]*x + p[1]
+            return p[0]*np.exp(-p[1]*x) + p[2]
 
     lm_func = _LMFunction(Foo().func, args=(x,))
     assert_(__py_verify_funcs(lm_func, p, m, n))
 
 
 def test_valid_class_method():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
 
     class Foo(object):
         @classmethod
         def func(cls, p, x):
-            return p[0]*x + p[1]
+            return p[0]*np.exp(-p[1]*x) + p[2]
 
     lm_func = _LMFunction(Foo.func, args=(x,))
     assert_(__py_verify_funcs(lm_func, p, m, n))
 
 
 def test_func_not_callable():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
     func = []
 
@@ -73,34 +74,34 @@ def test_func_not_callable():
 
 
 def test_func_invalid_args_given():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
-    func = lambda p, x : p[0]*x + p[1]
+    func = lambda p, x : p[0]*np.exp(-p[1]*x) + p[2]
 
     lm_func = _LMFunction(func, args=())
     assert_raises(LMUserFuncError, __py_verify_funcs, lm_func, p, m, n)
 
 
 def test_func_returns_invalid_size():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
-    func = lambda p, x : p[0]*x + p[1]
+    func = lambda p, x : p[0]*np.exp(-p[1]*x) + p[2]
 
     lm_func = _LMFunction(func, args=(x,))
     assert_raises(LMUserFuncError, __py_verify_funcs, lm_func, p, m, n+1)
 
 
 def test_jacf_returns_invalid_size():
-    m, n = 2, 50
-    p = np.array([1, 2])
+    m, n = 3, 50
+    p = np.array([1, 2, 3])
     x = np.linspace(-5, 5, n)
-    func = lambda p, x : p[0]*x + p[1]
+    func = lambda p, x : p[0]*np.exp(-p[1]*x) + p[2]
     def jacf(p, x):
-        y = np.empty((n+1,m))
-        y[:,0] = x
-        y[:,1] = 1
+        y = np.empty((n,m-1))
+        y[:,0] = np.exp(-p[1]*x)
+        y[:,1] = -p[0] * x * np.exp(-p[1]*x)
         return y
 
     lm_func = _LMFunction(func, args=(x,),  jacf=jacf)
